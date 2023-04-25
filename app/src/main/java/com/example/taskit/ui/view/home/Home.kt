3169103a1350fbef761e60c22b7550481f7e96b4
Arnaud <arnaud.a.Durand@gmail.com>
2023-04-25 18:08:42 +0300

    + home buttons redesign

--- app/src/main/java/com/example/taskit/ui/view/home/Home.kt
+++ app/src/main/java/com/example/taskit/ui/view/home/Home.kt
@@ -62,18 +62,19 @@ fun MainTopBar(title: String, navController: NavController){
 fun TwoButtonRow(navController: NavController){
     MaterialTheme {
         Row(
-            horizontalArrangement = Arrangement.SpaceEvenly,
+            horizontalArrangement = Arrangement.SpaceBetween,
             modifier = Modifier.fillMaxWidth()
         ) {
             Button(
                 onClick = { navController.navigate("Offers") },
-                modifier = Modifier.padding(16.dp)
+                modifier = Modifier.padding(16.dp).size(width = 170.dp, height = 40.dp)
+                ,
             ) {
                 Text("My Offers")
             }
             Button(
                 onClick = { navController.navigate("NewOffer") },
-                modifier = Modifier.padding(16.dp),
+                modifier = Modifier.padding(16.dp).size(width = 170.dp, height = 40.dp),
                 colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFFFA500)),
             ) {
                 Text("Add an offer")
