ab1d34ddb4bf95c7eb907fe80d749ccd3bdd7417
imraghavbnsal007 <raghavb025@gmail.com>
2023-04-28 15:03:13 +0300

    profile image upload to storge
    Firebase have been added now for the image

--- app/src/main/java/com/example/taskit/ui/view/profile/Pimage.kt
+++ app/src/main/java/com/example/taskit/ui/view/profile/Pimage.kt
@@ -1,66 +1,27 @@
 package com.example.taskit.ui.view.profile
 
 import android.net.Uri
-import android.widget.Toast
+import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
 import com.example.taskit.R
-import com.example.taskit.ui.theme.TaskitTheme
-
-@Composable
-fun ProfileScreen1(navController: NavController.Companion) {
-
-    val notification = rememberSaveable { mutableStateOf("") }
-    if (notification.value.isNotEmpty()) {
-        Toast.makeText(LocalContext.current, notification.value, Toast.LENGTH_LONG).show()
-        notification.value = ""
-    }
-
-    var name by rememberSaveable { mutableStateOf("default name") }
-    var username by rememberSaveable { mutableStateOf("default username") }
-    var bio by rememberSaveable { mutableStateOf("default bio") }
-
-    Column(
-        modifier = Modifier
-            .verticalScroll(rememberScrollState())
-            .padding(8.dp)
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(text = "Cancel",
-                modifier = Modifier.clickable { notification.value = "Cancelled" })
-            Text(text = "Save",
-                modifier = Modifier.clickable { notification.value = "Profile updated" })
-        }
-
-        ProfileImage1(navController = NavController)
-    }
-}
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 @Composable
 fun ProfileImage1(navController: NavController.Companion) {
@@ -68,10 +29,22 @@ fun ProfileImage1(navController: NavController.Companion) {
     val painter = rememberAsyncImagePainter(
         imageUri.value.ifEmpty { R.drawable.ic_user }
     )
+    var imageRef = Firebase.storage.reference.child("profile.jpg")
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.GetContent()
     ) { uri: Uri? ->
         uri?.let { imageUri.value = it.toString() }
+        uri?.let{ u ->
+            imageRef.putFile(u)
+                .addOnSuccessListener {
+                    imageRef.downloadUrl.addOnSuccessListener { remoteUri ->
+                        Log.d("***", remoteUri.toString())
+                    }
+                }
+                .addOnFailureListener{
+                    Log.e("***", it.message.toString())
+                }
+        }
     }
 
     Column(
